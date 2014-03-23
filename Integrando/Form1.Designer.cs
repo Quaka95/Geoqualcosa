54a2c0070254da71453bec84f98015ed8874aac0
Giacomo Garabello <giacomo.garabello@vallauri.edu>
2014-03-23 18:59:10 +0100

    Creazione Progetto

---
+++ Integrando/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Integrando
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graph1 = new Mate.Graph.Graph();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textRegex1 = new Mate.TextRegex.TextRegex();
+            this.textRegex2 = new Mate.TextRegex.TextRegex();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graph1
+            // 
+            this.graph1._enableZoomingPanning = true;
+            this.graph1._xMax = 10D;
+            this.graph1._xMin = -10D;
+            this.graph1._yMax = 10D;
+            this.graph1._yMin = -10D;
+            this.graph1.Location = new System.Drawing.Point(0, 0);
+            this.graph1.Name = "graph1";
+            this.graph1.Size = new System.Drawing.Size(400, 400);
+            this.graph1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 548);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textRegex1
+            // 
+            this.textRegex1.Location = new System.Drawing.Point(122, 425);
+            this.textRegex1.Name = "textRegex1";
+            this.textRegex1.Regex = "^[-]?[0-9]+[,]?[0-9]*$";
+            this.textRegex1.SafeText = "";
+            this.textRegex1.Size = new System.Drawing.Size(200, 20);
+            this.textRegex1.TabIndex = 2;
+            // 
+            // textRegex2
+            // 
+            this.textRegex2.Location = new System.Drawing.Point(122, 452);
+            this.textRegex2.Name = "textRegex2";
+            this.textRegex2.Regex = "^[-]?[0-9]+[,]?[0-9]*$";
+            this.textRegex2.SafeText = "";
+            this.textRegex2.Size = new System.Drawing.Size(200, 20);
+            this.textRegex2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(122, 479);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(200, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 431);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Valore A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valore B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 486);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Iterazioni";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 519);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Integra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 568);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textRegex2);
+            this.Controls.Add(this.textRegex1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.graph1);
+            this.Name = "Form1";
+            this.Text = "Integrando";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Mate.Graph.Graph graph1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Mate.TextRegex.TextRegex textRegex1;
+        private Mate.TextRegex.TextRegex textRegex2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Integrando/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using info.lundin.math;
+using Mate.Graph;
+
+namespace Integrando
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double fx(double x, string f)   //Funzione per il calcolo del valore di una variabile in un punto
+        {
+            Hashtable table = new Hashtable();  //Creo una HashTable
+            table.Add("x", x.ToString());   //imposto in X il valore da determinare
+            table.Add("e", Math.E.ToString());  //imposto in e il numero di Nepero
+            ExpressionParser _parser = new ExpressionParser();
+            return _parser.Parse(f, table); //Calcolo il valore di x nella funzione
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ';')
+            {
+                graph1._clearAll();
+                graph1._drawAxis();
+                graph1._drawGridFixed(1, 1);
+                try
+                {
+                    graph1._drawFunction(textBox1.Text, 100, new Pen(Color.Red));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("La funzione é errata!!");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a;
+            double b;
+            string f = textBox1.ToString();
+            try
+            {
+                a = Convert.ToDouble(textRegex1.SafeText);
+                b = Convert.ToDouble(textRegex2.SafeText);
+                if (a > b)
+                {
+                    double c = a;
+                    a = b;
+                    b = c;
+                }
+                int n = (int)numericUpDown1.Value;
+                double step = (b - a) / n;
+                for (int i = 0; i < n; i++) 
+                {
+                    double po = a + (step * i);
+                    if (fx(po, f) * fx(po+step, f) > 0)
+                    {
+                        graph1._drawRectangle(po,(fx(po, f)>0)?0:fx(po, f),step,fx(po,f),Color.Red);
+                    }
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Controlla i vaolori inseriti!!");
+            }
+        }
+    }
+}

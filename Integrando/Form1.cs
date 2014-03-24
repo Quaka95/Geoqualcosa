c5e65e889e918265955704a4c87904c8db742a37
Giacomo Garabello <giacomo.garabello@vallauri.edu>
2014-03-24 12:09:21 +0100

    Refactor e debug

--- Integrando/Form1.cs
+++ Integrando/Form1.cs
@@ -49,35 +49,57 @@ namespace Integrando
             }
         }
 
+        bool dati_ok()
+        {
+            bool ret;
+            ret = false;
+            ret = ret && textRegex1.Match;
+            ret = ret && textRegex2.Match;
+            return ret;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            if (flag)
+
+            const double VERY_LITTLE_NUMBER = 0.00000001;
+
+            if (flag && dati_ok()) // _funzione_disegnata // se la funzione é già stata disegnata
             {
-                double a;
-                double b;
-                double ins = 0;
-                double cir = 0;
-                string f = textBox1.Text;
-                try
+                double a;  // il punto a sx dell'integrale
+                double b;  // il punto a dx dell'integrale
+                double ins = 0; // l'area delimitata dai rettangoli inferiori
+                double cir = 0; // l'area delimitata dai rettangoli superiori
+                string f = textBox1.Text;  // la stringa con la funzione
+                // preleva i dati dalla form
+                a = Convert.ToDouble(textRegex1.SafeText);
+                b = Convert.ToDouble(textRegex2.SafeText);
+                if (a > b)
+                // nel caso in cui i dati xa ed xb non siano compatibili li swappa
                 {
-                    a = Convert.ToDouble(textRegex1.SafeText);
-                    b = Convert.ToDouble(textRegex2.SafeText);
-                    if (a > b)
+                    double c = a;
+                    a = b;
+                    b = c;
+                }
+                int n = (int)numericUpDown1.Value;  // preleva il numero di poligoni dalla form
+                double step = (b - a) / n;  // la larghezza del singolo poligono
+
+                // double deltax = (xb - xa) / n;
+
+                for (double x = a; x <= b; x += step)
+
+
+                //for (int i = 0; i < n; i++)
+                {
+                    double po = x;  // l'ascissa del punto dell'angolo in basso a sinistra del rettangolo
+                    double fpo = fx(x, f);      // l'ordinata del punto dell'angolo in basso a sinistra del rettangolo
+                    double pos = x + step;     // l'ascissa del punto in basso a destra del rettangolo
+                    double fpos = fx(x + step, f); // l'ordinata del punto in basso a destra del rettangolo
+
+                    // TODO: utilizzare tre checkboxes per le tre tipologie di aree
+                    try
                     {
-                        double c = a;
-                        a = b;
-                        b = c;
-                    }
-                    int n = (int)numericUpDown1.Value;
-                    double step = (b - a) / n;
-                    for (int i = 0; i < n; i++)
-                    {
-                        double po = a + (step * i);
-                        double fpo = fx(po, f);
-                        double pos = po + step;
-                        double fpos = fx(po + step, f);
                         if (fpo * fpos > 0 && ((fpo > 0) ? fpo : -(fpo)) > ((fpos > 0) ? fpos : -(fpos)))
                         {
+                            // rettangoli la cui base inferiore poggia sull'asse delle ascisse
                             graph1._drawRectangle(po, (fpo >= 0) ? fpo : 0, step, (fpo > 0) ? fpo : -(fpo), Color.Red);
                             cir += step * fpo;
                             graph1._drawRectangle(po, (fpos >= 0) ? fpos : 0, step, (fpos > 0) ? fpos : -(fpos), Color.Blue);
@@ -85,26 +107,25 @@ namespace Integrando
                         }
                         else if (((fpo > 0) ? fpo : -(fpo)) < ((fpos > 0) ? fpos : -(fpos)))
                         {
+                            // rettangoli la cui base superiore poggia sull'asse delle ascisse
                             graph1._drawRectangle(po, (fpos >= 0) ? fpos : 0, step, (fpos > 0) ? fpos : -(fpos), Color.Red);
                             cir += step * fpos;
                             graph1._drawRectangle(po, (fpo >= 0) ? fpo : 0, step, (fpo > 0) ? fpo : -(fpo), Color.Blue);
                             ins += step * fpo;
                         }
                     }
-                    if (cir < 0.00000001)
-                        cir = 0;
-                    if (ins < 0.00000001)
-                        ins = 0;
-                    MessageBox.Show("Circoscritti: " + cir.ToString() + "\nInscritti: " + ins.ToString());
+
+                    catch (ClassGraph.GraphException ex) { }
+                    catch (OverflowException ex) { MessageBox.Show("La funzione molto probabilmente in uno dei punti non esiste!!!"); }
+                    //TODO: Capire perché se passo da (0,0) non funziona...
                 }
-                catch (FormatException ex)
-                {
-                    MessageBox.Show("Controlla i vaolori inseriti!!\n" + ex.Message);
-                }
-                catch (ClassGraph.GraphException ex) { }
-                catch (NullReferenceException ex) { MessageBox.Show(ex.Message); }; //TODO: Capire perché se passo da (0,0) non funziona...
+                if (cir < VERY_LITTLE_NUMBER && -(cir) < VERY_LITTLE_NUMBER)
+                    cir = 0;
+                if (ins < VERY_LITTLE_NUMBER && -(ins) < VERY_LITTLE_NUMBER)
+                    ins = 0;
+                MessageBox.Show("Circoscritti: " + cir.ToString() + "\nInscritti: " + ins.ToString());
             }
-            else 
+            else
             {
                 MessageBox.Show("Se disegni qualcosa forse è meglio...");
             }
